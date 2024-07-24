2f5211e1fefe90f76ee426f9dbf252dafc2cf302
DaBetterman <1ebrahimr@gmail.com>
2024-07-24 21:42:31 +0200

    0x01. React intro

--- 0x01-react_intro/task_0/dashboard/src/App.js
+++ 0x01-react_intro/task_0/dashboard/src/App.js
@@ -1,20 +1,21 @@
+import React from 'react';
+import holbertonLogo from './holberton-logo.png';
 import './App.css';
-import holberton_logo from './holberton-logo.jpg';
 
 function App() {
   return (
-    <>
-      <div className="App-header">
-        <img src={holberton_logo} alt="logo" />
+    <div className="App">
+      <header className="App-header">
+        <img src={holbertonLogo} className="App-logo" alt="Holberton Logo" />
         <h1>School dashboard</h1>
-      </div>
+      </header>
       <div className="App-body">
         <p>Login to access the full dashboard</p>
       </div>
-      <div className="App-footer">
+      <footer className="App-footer">
         <p>Copyright 2020 - Holberton School</p>
-      </div>
-    </>
+      </footer>
+    </div>
   );
 }
 
